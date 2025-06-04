f35a6b54fdb399888b2ad2b790404bbe57ed5c35
Artur Taskaev <taskaniartur@gmail.com>
2025-06-04 22:15:46 +0300

    fixed connection to server, now 1 connection per start

--- server-socket/src/main/java/ru/itmo/socket/server/Server.java
+++ server-socket/src/main/java/ru/itmo/socket/server/Server.java
@@ -6,7 +6,6 @@ import ru.itmo.socket.common.util.SocketContext;
 import ru.itmo.socket.server.commands.ServerCommand;
 import ru.itmo.socket.server.commands.ServerCommandContext;
 import ru.itmo.socket.server.commands.impl.CommandHistory;
-import ru.itmo.socket.server.commands.impl.ExitCommand;
 import ru.itmo.socket.server.manager.LabWorkTreeSetManager;
 import ru.itmo.socket.server.manager.XmlCollectionLoader;
 
@@ -20,6 +19,10 @@ import java.net.Socket;
 public class Server {
 
     public static void main(String[] args) {
+        startServer();
+    }
+
+    private static void startServer() {
         // загружаем из файла collection.txt изначальные значения
         LabWorkTreeSetManager manager = LabWorkTreeSetManager.getInstance();
         new XmlCollectionLoader(manager, "collection.txt").load();
@@ -30,15 +33,30 @@ public class Server {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен и ожидает подключения...");
 
-            // в цикле ждем подключения клиентов
-            while (true) {
-                if (!sendToClient(serverSocket)) {
-                    break;
-                }
-            }
+            // ждем подключений
+            acceptConnection(serverSocket);
+
         } catch (Exception e) {
             System.err.println("Ошибка сервера: " + e.getMessage());
             e.printStackTrace();
+        }
+    }
+
+    private static void acceptConnection(ServerSocket serverSocket) throws IOException, ClassNotFoundException {
+        try (Socket clientSocket = serverSocket.accept();
+             ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+             ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream())) {
+
+            System.out.println("Клиент подключился: " + clientSocket.getInetAddress());
+
+            while (true) {
+                // в цикле обрабатываем команды с клиентской стороны подключения клиентов
+                if (!sendToClient(ois, oos)) {
+                    break;
+                }
+            }
+        } catch (EOFException eofException) {
+            System.err.println("Клиент неожиданно прекратил соединение, не удалось прочитать команду");
         }
     }
 
@@ -47,47 +65,39 @@ public class Server {
      *
      * @return true - если любая команда кроме 'exit', false - если 'exit'
      */
-    private static boolean sendToClient(ServerSocket serverSocket) throws IOException, ClassNotFoundException {
+    private static boolean sendToClient(ObjectInputStream ois, ObjectOutputStream oos) throws IOException, ClassNotFoundException {
         boolean continueWorking = true;
-        try (Socket clientSocket = serverSocket.accept();
-             ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
-             ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream())) {
 
-            System.out.println("Клиент подключился: " + clientSocket.getInetAddress());
+        // Чтение запроса от клиента (в объект)
+        CommandDto commandDto = (CommandDto) ois.readObject();
+        System.out.println("Получено от клиента: " + commandDto);
 
-            // Чтение запроса от клиента (в объект)
-            CommandDto commandDto = (CommandDto) ois.readObject();
-            System.out.println("Получено от клиента: " + commandDto);
+        String commandName = commandDto.getCommandName();
+        ServerCommand serverCommand = ServerCommandContext.getCommand(commandName);
 
-            String commandName = commandDto.getCommandName();
-            ServerCommand serverCommand = ServerCommandContext.getCommand(commandName);
+        if (serverCommand != null) {
+            // кидаем клиенту количество строк вывода (которое ему надо будет считать) из-за СКРИПТА (команда execute_script),
+            // потому что в скрипте может быть много команд :(
+            // у других команд по умолчанию - 1 строка вывода, можно если че переопределить для любой команды
+            int numberOfOutputLines = serverCommand.getNumberOfOutputLines(commandDto.getArg());
+            oos.writeUTF(String.valueOf(numberOfOutputLines));
 
-            if (serverCommand != null) {
-                // кидаем клиенту количество строк вывода (которое ему надо будет считать) из-за СКРИПТА (команда execute_script),
-                // потому что в скрипте может быть много команд :(
-                // у других команд по умолчанию - 1 строка вывода, можно если че переопределить для любой команды
-                int numberOfOutputLines = serverCommand.getNumberOfOutputLines(commandDto.getArg());
-                oos.writeUTF(String.valueOf(numberOfOutputLines));
-
-                try {
-                    // выполняем команду!
-                    serverCommand.execute(oos, commandDto.getArg());
-                }
-                catch (AppExitException exitException) {
-                    // AppExitException бросается если команда ExitCommand
-                    // (через Exception, потому что в скрипте может быть exit => надо обработать сразу же)
-                    continueWorking = false;
-                }
-                CommandHistory.addCommand(commandName);
-            } else {
-                System.out.println("Команда не найдена! Введите 'help' для получения списка команд.");
+            try {
+                // выполняем команду!
+                serverCommand.execute(oos, commandDto.getArg());
+            } catch (AppExitException exitException) {
+                // AppExitException бросается если команда ExitCommand
+                // (через Exception, потому что в скрипте может быть exit => надо обработать сразу же)
+                continueWorking = false;
             }
-            oos.flush();
-            System.out.println("Отправлено клиенту вывод команды: " + commandName);
-
-        } catch (EOFException eofException) {
-            System.err.println("Клиент неожиданно прекратил соединение, не удалось прочитать команду");
+            CommandHistory.addCommand(commandName);
+        } else {
+            System.out.println("Команда не найдена! Введите 'help' для получения списка команд.");
         }
+        oos.flush();
+        System.out.println("Отправлено клиенту вывод команды: " + commandName);
+
+
         return continueWorking;
     }
 }
